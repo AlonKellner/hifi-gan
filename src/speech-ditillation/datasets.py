017ae22ce6fb7881b5a7396a6ae05a21ae16f9fc
Alon Kellner <4ubalon@gmail.com>
2021-07-14 12:59:56 +0300

    fixes

---
+++ src/speech-ditillation/datasets.py
@@ -0,0 +1,53 @@
+import random
+
+import torch
+import torch.utils.data
+from librosa.util import normalize
+
+from src.meldataset import load_wav
+
+MAX_WAV_VALUE = 32768.0
+
+
+class WaveDataset(torch.utils.data.Dataset):
+    def __init__(self, training_files, segment_size, sampling_rate, split=True, n_cache_reuse=1,
+                 fine_tuning=False):
+        self.audio_files = training_files
+        self.segment_size = segment_size
+        self.sampling_rate = sampling_rate
+        self.split = split
+        self.n_cache_reuse = n_cache_reuse
+        self.fine_tuning = fine_tuning
+
+    def __getitem__(self, index):
+        filename = self.audio_files[index]
+        if self._cache_ref_count == 0:
+            audio, sampling_rate = load_wav(filename)
+            audio = audio / MAX_WAV_VALUE
+            if not self.fine_tuning:
+                audio = normalize(audio) * 0.95
+            self.cached_wav = audio
+            if sampling_rate != self.sampling_rate:
+                raise ValueError("{} SR doesn't match target {} SR".format(
+                    sampling_rate, self.sampling_rate))
+            self._cache_ref_count = self.n_cache_reuse
+        else:
+            audio = self.cached_wav
+            self._cache_ref_count -= 1
+
+        audio = torch.FloatTensor(audio)
+        audio = audio.unsqueeze(0)
+
+        if not self.fine_tuning:
+            if self.split:
+                if audio.size(1) >= self.segment_size:
+                    max_audio_start = audio.size(1) - self.segment_size
+                    audio_start = random.randint(0, max_audio_start)
+                    audio = audio[:, audio_start:audio_start+self.segment_size]
+                else:
+                    audio = torch.nn.functional.pad(audio, (0, self.segment_size - audio.size(1)), 'constant')
+
+        return audio.squeeze(0), filename
+
+    def __len__(self):
+        return len(self.audio_files)
